afbbe3f3a0764d98a431190f8e74495f8ef34915
narekchang <narek.changlyan@gmail.com>
2015-08-04 20:00:07 +0600

    merge

--- cs/js/main.js
+++ cs/js/main.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
 	$(".content .table_hedear .search .search_text").change(function(){
 		if($(this).val()==""){$(this).removeClass("active")}else{$(this).addClass("active")};
 	});
+	$(".call_modal").on("click", function(){
+
+		var modalName = "#" + $(this).data("modal-name");
+
+		$(".wrapper").addClass("blur");
+
+		$(modalName).addClass("show");
+	});
+	$(".close_btn").on("click", function(){
+		$(".wrapper").removeClass("blur");
+
+		$(this).parent().removeClass("show");
+	});
 });
